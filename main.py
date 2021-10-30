c930966c28e8492d55ade50f919f166816c19986
AppiGarg <75184500+AppiGarg@users.noreply.github.com>
2021-10-30 20:36:35 +0000

    Added response

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def Code():
   print("Hello World")
-  Question = input("Do you like this website?(y)")
+  Question = input("Do you like this website?(y/n) ")
   if Question == "y":
     print("cool")
   else:
     print("awwww")
+  print("I think this is a cool website")
+  
